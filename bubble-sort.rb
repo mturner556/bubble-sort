59029dd47df12da21b0332b454f9ef4bcfb05f79
Matt Turner <mattturner1911@gmail.com>
2022-10-20 23:09:19 -0400

    Completed project. Used a combination of the #each_with_index loop and nested if statements. It does add a bunch of nil values to the array, and this is why I used the #delete method to get rid of them and return the sorted array.

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,1 +1,13 @@
-p "hello world"
+def bubble_sort (array)
+  array.each_with_index do |e, i|
+    if i < array.length - 1
+      if e > array[i + 1]
+        p array.insert(array[i + 1], array.delete_at(array[i]))
+      end 
+    end   
+  end
+  array.delete(nil)
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2])
